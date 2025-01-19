f5c5e5cc25695e39d3063c7495a37a0f14b5e35c
xforcevesa <nomodeset@qq.com>
2025-01-19 09:47:21 +0800

    misc

--- python/MoFA_marscode/start_marscode.sh
+++ python/MoFA_marscode/start_marscode.sh
@@ -2,7 +2,7 @@ conda activate mofa
 
 cd shopping_agents/
 dora up && dora build shopping_dataflow.yml && dora start shopping_dataflow.yml --detach
-setsid nohup hitl-agent &
+rm nohup.out; setsid nohup hitl-agent &
 cd ../ui
 streamlit run socket_client.py
 
